79f73e904ea2e0d5edbc8f41b57d7c23303a34a1
Brian Johnsen <info@brianjohnsen.dk>
2021-02-23 12:17:15 +0100

    adds new docker install guide

--- scripts/setup_apt-get.bash
+++ scripts/setup_apt-get.bash
@@ -106,11 +106,14 @@ sudo apt install -y network-manager-openconnect-gnome network-manager-openconnec
 # Docker
 ## See: https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-18-04
 ## and: https://docs.docker.com/install/linux/docker-ce/ubuntu/#upgrade-docker-after-using-the-convenience-script
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt update
-sudo apt install -y docker-ce
+#sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+#sudo apt update
+#sudo apt install -y docker-ce
+##
+## https://www.omgubuntu.co.uk/how-to-install-docker-on-ubuntu-20-04
+##
 
 ## VirtualBox (required by vagrant)
 #sudo apt install -y virtualbox virtualbox-ext-pack
