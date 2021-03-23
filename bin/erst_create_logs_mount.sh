32e60ee7c44a9531a47d079dfc847feab3a90512
Brian Johnsen <info@brianjohnsen.dk>
2021-03-23 14:48:03 +0100

    adds logs-all mount point

--- bin/erst_create_logs_mount.sh
+++ bin/erst_create_logs_mount.sh
@@ -5,6 +5,7 @@ sudo mkdir -v -p /media/nc/logs-nine
 if grep -qs '/logs-nine ' /proc/mounts; then
     echo "'/media/nc/logs-nine' is already mounted"
 else
-  sudo mount -v -t cifs -o credentials=/home/johnsen/.smbcredentials_nine_logs,uid=1000,vers=2.0 //samba.nonprod.es.local/logs-nine /media/nc/logs-nine
+  sudo mount -v -t cifs -o credentials=~/.smbcredentials_nine_logs,uid=1000,vers=2.0 //samba.nonprod.es.local/logs-nine /media/nc/logs-nine
+  sudo mount -v -t cifs -o credentials=~/.smbcredentials_nine_logs,uid=1000,vers=2.0 //samba.nonprod.es.local/logs-all /media/nc/logs-all
 fi
 
