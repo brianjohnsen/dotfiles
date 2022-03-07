2cab0896e676475af3e08fb64d574d138ba16d27
Brian Johnsen <info@brianjohnsen.dk>
2022-03-07 11:00:19 +0100

    adds better execute-last-shell-cmd

--- bin/execute-last-shell-cmd.sh
+++ bin/execute-last-shell-cmd.sh
@@ -1,30 +1,35 @@
 #!/usr/bin/env bash
 
-## window where we came from
-#ORIGIN=$(xdotool getactivewindow)
-#
-## find PID of Terminal
-##wid=$(xdotool search --onlyvisible --name "Terminal")
-#WID=$(xdotool search --onlyvisible --class gnome-terminal)
-#
-## activate Terminal window
-#xdotool windowactivate --sync $WID
-#
-## send keystrokes
-#xdotool key 'ctrl+p' 'Return'
-#
-## activate origin window
-#xdotool windowactivate --sync $ORIGIN
+# window where we came from
+ORIGIN=$(xdotool getactivewindow)
+
+# find PID of Terminal
+#wid=$(xdotool search --onlyvisible --name "Terminal")
+WID=$(xdotool search --onlyvisible --class gnome-terminal)
+
+# activate Terminal window
+xdotool windowactivate --sync $WID
+
+sleep .2
+
+# send keystrokes
+xdotool key 'Up'
+sleep .2
+xdotool key 'Return'
+sleep .2
+
+# activate origin window
+xdotool windowactivate --sync $ORIGIN
 
 
 
 
-WINDOW_NAME=$(xdotool getwindowfocus getwindowname)
-
-if echo $WINDOW_NAME | grep -q 'IDEA'; then
-  wmctrl -a Terminal
-  sleep .2
-  xdotool key Up
-  sleep .3
-  xdotool key Return
-fi
+##WINDOW_NAME=$(xdotool getwindowfocus getwindowname)
+##
+##if echo $WINDOW_NAME | grep -q 'IDEA'; then
+##fi
+#  wmctrl -a Terminal
+#  sleep .2
+#  xdotool key Up
+#  sleep .3
+#  xdotool key Return
